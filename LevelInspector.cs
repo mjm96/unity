1eb7249af5826951b0c770dd9f185e84e0dd2433
mjm96 <30803422+mjm96@users.noreply.github.com>
2017-08-20 13:40:17 +0430

    editor_first

---
+++ LevelInspector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MyObject))]
+//[CanEditMultipleObjects]
+public class LevelInspector : Editor
+{
+	public MyObject targetLevel;
+	SerializedProperty tex;
+
+    public void OnEnable()
+    {
+		targetLevel = (MyObject)target;
+		tex = serializedObject.FindProperty ("tx");
+    }
+    public override void OnInspectorGUI()
+    {
+		serializedObject.Update ();
+
+		targetLevel.position= EditorGUILayout.Vector3Field ("position", targetLevel.position);
+		 EditorGUILayout.ObjectField (tex, typeof(Texture));
+	
+		//EditorGUI.DrawTextureAlpha ();
+		serializedObject.ApplyModifiedProperties ();
+      
+    }
+}
---
+++ MyObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode] 
+
+public class MyObject : MonoBehaviour {
+
+
+	public Vector3 position;
+	public Vector3 rotation;
+	public Texture tx;
+	public string name1;
+	public Vector3 cff;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = position;
+		this.guiTexture = tx;
+	}
+}
